6deac9192bf930526b04a440fcdfb39b3715bccb
Piyush <piyushdravyakar48@gmail.com>
2024-03-31 19:36:12 +0530

    test practice

---
+++ test_cases/test_practice.py
@@ -0,0 +1,10 @@
+class Testgood:
+
+    def test_first(self):
+        print("This is first program")
+
+    def test_second(self):
+        print("This is second program")
+
+    def test_Third(self):
+        print("This is Third program")
